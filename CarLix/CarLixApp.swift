2acfcc6997e3e33aaf53f8d52c977194d4af9447
Illia Verezei <useraccont1507@gmail.com>
2025-03-17 18:11:09 +0200

    Initial Commit

---
+++ CarLix/CarLixApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarLixApp.swift
+//  CarLix
+//
+//  Created by Illia Verezei on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CarLixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
