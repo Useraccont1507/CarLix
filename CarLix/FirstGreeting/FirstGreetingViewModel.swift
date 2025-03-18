b2c2fdef1425ad2b71caaa0a56222b2e89420d8d
Illia Verezei <useraccont1507@gmail.com>
2025-03-18 17:15:25 +0200

    added FirstGreetingViewModel connection with NotoficationService

--- CarLix/FirstGreeting/FirstGreetingViewModel.swift
+++ CarLix/FirstGreeting/FirstGreetingViewModel.swift
@@ -8,9 +8,15 @@
 import Foundation
 
 final class FirstGreetingViewModel: ObservableObject {
+    private var notificationService: NotificationServiceProtocol?
+    
     @Published var greetingStep: GreetingStep = .begin
     @Published var notificationAlertState = false
     @Published var notificationDismissedAlertState = false
+    
+    init(notificationService: NotificationServiceProtocol? = nil) {
+        self.notificationService = notificationService
+    }
     
     func nextStep() {
         switch greetingStep {
@@ -25,12 +31,17 @@ final class FirstGreetingViewModel: ObservableObject {
         case .receiveNotification:
             
             if notificationAlertState {
-                requestNotification { isGranted in
-                    if isGranted {
-                        greetingStep = .end
-                    } else {
-                        notificationAlertState = false
-                        notificationDismissedAlertState = true
+                requestNotification { [weak self] isAllowed in
+                    guard let self = self else { return }
+                    
+                    DispatchQueue.main.async {
+                        if isAllowed {
+                            self.notificationAlertState = false
+                            self.greetingStep = .end
+                        } else {
+                            self.notificationAlertState = false
+                            self.notificationDismissedAlertState = true
+                        }
                     }
                 }
             } else if notificationDismissedAlertState {
@@ -45,8 +56,7 @@ final class FirstGreetingViewModel: ObservableObject {
         }
     }
     
-    func requestNotification(completion: ((Bool) -> Void)) {
-        //service
-        completion(false)
+    func requestNotification(completion: @escaping ((Bool) -> Void)) {
+        notificationService?.makeRequest { completion($0) }
     }
 }
