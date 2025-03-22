c7917b14a725870a71acbf5dcae260f0acbf5488
Illia Verezei <useraccont1507@gmail.com>
2025-03-22 20:21:08 +0200

    created appcoordinator

---
+++ CarLix/AppCoordinator/AppCoordinator.swift
@@ -0,0 +1,64 @@
+//
+//  AppCoordinator.swift
+//  CarLix
+//
+//  Created by Illia Verezei on 22.03.2025.
+//
+
+import SwiftUI
+
+protocol CoordinatorProtocol {
+    func start() -> AnyView
+}
+
+protocol AppCoordinatorProtocol: CoordinatorProtocol {
+    func switchToAuth()
+    func switchToMain()
+}
+
+enum MainViewType {
+    case greeting
+    case auth
+    case main
+}
+
+final class AppCoordinator: ObservableObject, AppCoordinatorProtocol {
+    
+    let notificationService: NotificationServiceProtocol?
+    let authService: AuthServiceProtocol?
+    let storageService: StorageServiceProtocol?
+    
+    @Published var mainViewType = MainViewType.main
+    
+    init(notificationService: NotificationServiceProtocol? = nil, authService: AuthServiceProtocol? = nil, storageService: StorageServiceProtocol? = nil) {
+        
+        self.notificationService = notificationService
+        self.authService = authService
+        self.storageService = storageService
+        
+        if !UserDefaults.standard.bool(forKey: "isEntered") {
+            mainViewType = .greeting
+            UserDefaults.standard.set(true, forKey: "isEntered")
+        } else {
+            if let auth = authService, auth.getIsUserEnterd() {
+                mainViewType = .main
+            } else {
+                mainViewType = .auth
+            }
+        }
+    }
+    
+    func start() -> AnyView {
+        AnyView(
+            AppCoordinatorFlow(coordinator: self)
+        )
+    }
+    
+    func switchToAuth() {
+        mainViewType = .auth
+    }
+    
+    func switchToMain() {
+        mainViewType = .main
+    }
+}
---
+++ CarLix/AppCoordinator/AppCoordinatorFlow.swift
@@ -0,0 +1,36 @@
+//
+//  AppCoordinatorFlow.swift
+//  CarLix
+//
+//  Created by Illia Verezei on 22.03.2025.
+//
+
+import SwiftUI
+
+struct AppCoordinatorFlow: View {
+    @ObservedObject var coordinator: AppCoordinator
+    
+    init(coordinator: AppCoordinator) {
+        self.coordinator = coordinator
+    }
+    
+    var body: some View {
+        ZStack {
+            switch coordinator.mainViewType {
+            case .greeting:
+                FirstGreeting(viewModel: FirstGreetingViewModel(coordinator: coordinator, notificationService: coordinator.notificationService))
+                    .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+            case .auth:  AuthCoordinator(appcoordinator: coordinator, authService: coordinator.authService, storageService: coordinator.storageService)
+                    .start()
+                    .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+            case .main: Second()
+                    .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+            }
+        }
+        .animation(.easeIn, value: coordinator.mainViewType)
+    }
+}
+
+#Preview {
+    AppCoordinatorFlow(coordinator: AppCoordinator())
+}
