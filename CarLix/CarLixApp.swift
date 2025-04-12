dbf819e74ba3d30bcac0168212f2d7bba304ebcd
Illia Verezei <useraccont1507@gmail.com>
2025-04-12 20:58:04 +0300

    modified app file

--- CarLix/CarLixApp.swift
+++ CarLix/CarLixApp.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct CarLixApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    let notificationService = NotificationService(delegate: NotificationDelegate())
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //FirstGreeting(viewModel: FirstGreetingViewModel(notificationService: notificationService))
+            //RegisterPhoneView(viewModel: SignUpViewModel())
+            if let authService = delegate.authService {
+                let keychainService = delegate.keychainService // Отримуємо keychainService
+                let storageService = StorageService(keychainService: keychainService!)
+                let coordinator = AppCoordinator(notificationService: notificationService, authService: authService, storageService: storageService)
+                coordinator.start()
+            } else {
+                Text("Initializing...")
+            }
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    var authService: AuthServiceProtocol?
+    var keychainService: KeychainServiceProtocol! // Зробимо keychainService доступним
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        self.keychainService = KeychainService() // ініціалізація keychain
+        self.authService = AuthService(keychainService: keychainService) // ініціалізація authService
+        return true
     }
 }
