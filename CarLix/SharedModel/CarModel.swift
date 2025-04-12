077bb20ae6407f67d059fef3915d906c7e617e74
Illia Verezei <useraccont1507@gmail.com>
2025-04-12 20:56:25 +0300

    created car, fuel, service models

---
+++ CarLix/SharedModel/CarModel.swift
@@ -0,0 +1,95 @@
+//
+//  CarModel.swift
+//  CarLix
+//
+//  Created by Illia Verezei on 04.04.2025.
+//
+
+import SwiftUI
+
+enum CarType: String, CaseIterable {
+    case hatchback
+    case sedan
+    case universal
+    case liftback
+    case cabrio
+    case truck
+    case moto
+}
+
+enum FuelType: String, CaseIterable {
+    case gasoline
+    case diesel
+    case gas
+}
+
+enum TypeOfDrive: String, CaseIterable {
+    case fwd
+    case rwd
+    case awd
+}
+
+enum TypeOfTransmission: String, CaseIterable {
+    case manual
+    case automatic
+}
+
+enum Currency {
+    case UAH
+    case USD
+}
+
+struct Car: Identifiable, Hashable {
+    var id: String
+    var image: UIImage?
+    var type: CarType
+    var name: String
+    var year: Int
+    var engineCode: String
+    
+    var engineName: String {
+        String(engineSize) + " " + (!engineCode.isEmpty ? engineCode : fuel.rawValue )
+    }
+    
+    var engineSize: Float
+    var fuel: FuelType
+    var carMileage: Int
+    var typeOfTransmission: TypeOfTransmission
+    var typeOfDrive: TypeOfDrive
+    var vinCode: String
+    
+    var fuels: [CarFuel]
+    var services: [CarService]
+}
+
+struct CarFuel: Hashable {
+    var id: String = UUID().uuidString
+    var liters: Int
+    var fuelType: FuelType
+    var currency: Currency = .UAH
+    var pricePerLiter: Float?
+    var price: Float? {
+        guard let pricePerLiter = pricePerLiter else { return nil }
+        return pricePerLiter * Float(liters)
+    }
+    var currentMileage: Int
+    var stationName: String?
+    var stationAddress: String?
+    var documents: Data?
+    var date: Date
+}
+
+struct CarService: Hashable {
+    var id: String = UUID().uuidString
+    var workDescription: String
+    var detailedDescription: String?
+    var currentMileage: Int
+    var currency: Currency = .USD
+    var price: Int
+    var stationName: String?
+    var stationAddress: String?
+    var documents: Data?
+    var date: Date
+    var isNotified: Bool
+    var notificationDate: Date?
+}
